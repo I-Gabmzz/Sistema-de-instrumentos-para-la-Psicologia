bc5106fa6d90c0c76301049535fc7f89601a1547
PC OSTRICH <a1197503@uabc.edu.mx>
2025-08-22 00:12:56 -0700

    Se declaran los atributos y el constructor de la clase control.

--- Control.java
+++ Control.java
@@ -1,2 +1,15 @@
+// Se declaran las librerias necesarias para esta clase.
+import java.util.ArrayList;
+
+// Se anuncia la creación de la clase.
 public class Control {
+    // Se declaran los atributos pertenecientes a la clase.
+    private ArrayList<Instrumento> instrumentos; // Conjunto de instrumentos que existen en el sistema.
+
+    // Se declara el metodo constructor del control.
+    public Control() {
+        instrumentos = new ArrayList<>();
+    }
+
+
 }
